2d1acf23287bf8c7bab753c07f0b81cf2daf8777
Hank Warkentin - WellSky <47421367+hlwarkentin@users.noreply.github.com>
2020-08-20 14:52:22 -0500

    Add duplicate file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace SonarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please analyze my code");
+            string newName = "This is your name: Hank";
+            if (newName == GetName("Hank"))
+            {
+                Console.WriteLine("This is real code, SonarQube!");
+            }
+        }
+
+        public static string GetName(string name)
+        {
+            return $"This is your name: {name}";
+        }
+    }
+}
